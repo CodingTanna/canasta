9f21ef7f552284a6d1d0f047a98ddfab5d5192f3
CodingTanna <tanna@posteo.de>
2020-05-08 14:59:01 +0200

    2 files modified, 1 added

--- src/app/stack.component.ts
+++ src/app/stack.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'stack',
-  template: `<h1>Ich bin der Stack #{{nummer}}</h1><ol *ngFor="let s of anzahl">{{s}}</ol><div>{{anzahl.length}}</div>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class StackComponent  {
