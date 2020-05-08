4377238040933466526a0a6a6da8988e7366ba1b
CodingTanna <tanna@posteo.de>
2020-05-08 09:31:47 +0200

    3 files modified

--- src/app/stack.component.ts
+++ src/app/stack.component.ts
@@ -2,9 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'stack',
-  template: `<h1>Ich bin der Stack #{{nummer}}</h1>`,
+  template: `<h1>Ich bin der Stack #{{nummer}}</h1><ol *ngFor="let s "></ol>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class StackComponent  {
   @Input() nummer: string;
+  @Input() anzahl: any[];
 }
